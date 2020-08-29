5b53eaac2e5a1c6242f4b23b779c052f824d8083
Khalid <mohamedkhaledshaaban@gmail.com>
2020-08-29 22:00:25 +0400

    add dashboard

---
+++ src/components/dashboard/AvailableCars.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const AvailableCars = () => {
+  return <h1>Available cars</h1>;
+};
+
+export default AvailableCars;
--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import "./Sidebar.scss";
 const Sidebar = () => {
-  return <div className="dashboard-sidebar bg-theme-light">sidebar</div>;
+  return (
+    <div className="dashboard-sidebar">
+      <ul className="list-unstyled">
+        <li>
+          <NavLink to="/dashboard/cars">Available cars</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dashboard/customers">Customers</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
